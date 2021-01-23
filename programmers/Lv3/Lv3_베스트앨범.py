d578d3d962fe9adf46ec95a90bcf32222068b565
minwooKang <rkdalsdn94@gmail.com>
2021-01-24 05:39:08 +0900

    밑에 방법으로 풀어보려고 했는데 통과안됨 나중에 다시 풀어보기

--- programmers/Lv3/Lv3_베스트앨범.py
+++ programmers/Lv3/Lv3_베스트앨범.py
@@ -32,3 +32,35 @@ def solution(genres, plays):
 
 print(solution(['classic', 'pop', 'classic', 'classic', 'pop'],
                [500, 600, 150, 800, 2500]))  # [4,1,3,0]
+# def solution(genres, plays):
+#     answer = []
+
+#     genre = {}
+#     dic = {}
+#     for i, (x, y) in enumerate(zip(genres, plays)):
+#         if i in genre:
+#             genre[x] += y
+#         else:
+#             genre[x] = y
+#         if x in dic:
+#             dic[x].append([y, i])
+#         else:
+#             dic[x] = []
+#             dic[x].append([y, i])
+
+#     genre = [i for i, j in sorted(
+#         genre.items(), key=lambda x: x[1], reverse=True)]
+#     temp = []
+#     for i in genre:
+#         temp = sorted(dic[i], key=lambda x: x[0], reverse=True)
+#         if len(temp) > 1:
+#             answer.append(temp[0][1])
+#             answer.append(temp[1][1])
+#         else:
+#             answer.append(temp[0][1])
+
+#     return answer
+
+
+# print(solution(['classic', 'pop', 'classic', 'classic', 'pop'],
+#                [500, 600, 150, 800, 2500]))  # [4,1,3,0]
