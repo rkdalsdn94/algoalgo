f316821b0febc37b48ded03a1be64669dddd7f4b
minwooKang <rkdalsdn94@gmail.com>
2021-01-23 21:19:53 +0900

    프로그래머스 Lv3 베스트앨범

--- README.md
+++ README.md
@@ -45,3 +45,4 @@
 | [Lv2. JadenCase 문자열 만들기](https://programmers.co.kr/learn/courses/30/lessons/12951) | [py](programmers/Lv2/JadenCase_문자열_만들기.py)|
 | [Lv2. 더 맵게](https://programmers.co.kr/learn/courses/30/lessons/42626) | [py](programmers/Lv2/Lv2_더_맵게.py)|
 | [Lv2. 소수 찾기](https://programmers.co.kr/learn/courses/30/lessons/42839) | [py](programmers/Lv2/Lv2_소수_찾기.py)|
+| [Lv3. 베스트앨범](https://programmers.co.kr/learn/courses/30/lessons/42579) | [py](programmers/Lv3/Lv3_베스트앨범.py)|
---
+++ programmers/Lv3/Lv3_베스트앨범.py
@@ -0,0 +1,34 @@
+def solution(genres, plays):
+    answer = []
+    genre = {}
+    for i, j in zip(genres, plays):
+        if i in genre:
+            genre[i] += j
+        else:
+            genre[i] = j
+
+    genre = [i for i, j in sorted(
+        genre.items(), key=lambda x: x[1], reverse=True)]
+
+    dic = {}
+    for i, j in enumerate(zip(genres, plays)):
+        if j[0] in dic:
+            dic[j[0]].append([j[1], i])
+        else:
+            dic[j[0]] = list()
+            dic[j[0]].append([j[1], i])
+
+    temp = []
+    for i in genre:
+        temp = sorted(dic[i], key=lambda x: x[0], reverse=True)
+        if len(temp) > 1:
+            answer.append(temp[0][1])
+            answer.append(temp[1][1])
+        else:
+            answer.append(temp[0][1])
+
+    return answer
+
+
+print(solution(['classic', 'pop', 'classic', 'classic', 'pop'],
+               [500, 600, 150, 800, 2500]))  # [4,1,3,0]
