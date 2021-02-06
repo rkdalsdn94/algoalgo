6d181ab21f849eb090ad1cc8e0665f257197584e
minwooKang <rkdalsdn94@gmail.com>
2021-02-06 17:11:56 +0900

    다른 사람 풀이에 너무 멋있는게 있어서 옮겨봄

--- programmers/Lv2/Lv2_타겟_넘버.py
+++ programmers/Lv2/Lv2_타겟_넘버.py
@@ -1,19 +1,27 @@
-from collections import deque
+# from collections import deque
 
-def solution(numbers, target):
-    answer = 0
-    q = deque()
-    q.append([0,0])
+# def solution(numbers, target):
+#     answer = 0
+#     q = deque()
+#     q.append([0,0])
     
-    while q:
-        a, b = q.popleft()
+#     while q:
+#         a, b = q.popleft()
         
-        if b == len(numbers):
-            if a == target:
-                answer += 1
-        else:
-            q.append([a+numbers[b], b+1])
-            q.append([a-numbers[b], b+1])      
+#         if b == len(numbers):
+#             if a == target:
+#                 answer += 1
+#         else:
+#             q.append([a+numbers[b], b+1])
+#             q.append([a-numbers[b], b+1])      
     
-    return answer
+#     return answer
+# print(solution([1,1,1,1,1], 3)) # 5
+
+# 위에 처럼 풀고 다른 사람 풀이에 이거 봤는데 진짜 예술이다...
+from itertools import product
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target) 
 print(solution([1,1,1,1,1], 3)) # 5
