7359f9a0fe059c1871f35e58d6c38eba39ac7564
minwooKang <rkdalsdn94@gmail.com>
2021-03-15 23:14:08 +0900

    코드 수정

--- baekjoon/적록색약_10026.py
+++ baekjoon/적록색약_10026.py
@@ -16,7 +16,6 @@ bfs_ck = [ [0] * n for _ in range(n) ]
 rg_ck = [ [0] * n for _ in range(n) ]
 
 def bfs(q):
-    global bfs_cnt
     while q:
         a, b, temp = q.popleft()
         bfs_ck[a][b] = 1
@@ -25,39 +24,39 @@ def bfs(q):
             nx, ny = a + i, b + j
 
             if 0 <= nx < n and 0 <= ny < n and n_list[nx][ny] == temp and bfs_ck[nx][ny] == 0:
-                # 위치인덱스, 해당 색, 체크에서 아직 들어오지 못했을때
-                bfs_ck[nx][ny] = 1
                 q.append((nx, ny, n_list[nx][ny]))
-    bfs_cnt += 1
+                bfs_ck[nx][ny] = 1
 
 def rg_bfs(q):
-    global rg_cnt
     while q:
         a, b, temp = q.popleft()
-        rg_ck[a][b] = 1
+        rg_ck[a][b] = 0
 
         for i, j in zip(dx, dy):
             nx, ny = a + i, b + j
 
-            if 0 <= nx < n and 0 <= ny < n and n_list[nx][ny] == temp and rg_ck[nx][ny] == 0:
-                rg_ck[nx][ny] = 1
-                q.append((nx, ny, n_list[nx][ny]))
-    rg_cnt += 1
+            if 0 <= nx < n and 0 <= ny < n and rg_ck[nx][ny] == temp:
+                q.append((nx, ny, rg_ck[nx][ny]))
+                rg_ck[nx][ny] = 0
 
 for i in range(n):
     for j in range(n):
-        if n_list[i][j] == 'G':
-            rg_ck[i][j] = 'R'
+        if n_list[i][j] == 'G' or n_list[i][j] == 'R':
+            rg_ck[i][j] = 1
+        else:
+            rg_ck[i][j] = 2
+        
 
 for i in range(n):
     for j in range(n):
-        # for z in ('R','G','B')
-        q.append((i, j, n_list[i][j]))
         if bfs_ck[i][j] == 0:
+            q.append((i, j, n_list[i][j]))
             bfs(q)
-        q.append((i, j, n_list[i][j]))
-        if rg_ck[i][j] == 0:
+            bfs_cnt += 1
+        if rg_ck[i][j] != 0:
+            q.append((i, j, rg_ck[i][j]))
             rg_bfs(q)
+            rg_cnt += 1
 
 # print(n_list)
 print(bfs_cnt, rg_cnt)
