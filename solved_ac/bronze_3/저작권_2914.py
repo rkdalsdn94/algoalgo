b77746e2e338642942255b77d29070926eb95d0f
minwooKang <rkdalsdn94@gmail.com>
2022-06-28 00:23:50 +0900

    refactor: 솔브드 브론즈3 저작권 2914 풀이 추가
    풀이 과정이 부족한거 같아서 파일 상단에 풀이 내용 추가
    입력값의 범위가 작아서 가능한 단순하게 푸는버 추가

--- solved_ac/bronze_3/저작권_2914.py
+++ solved_ac/bronze_3/저작권_2914.py
@@ -2,6 +2,16 @@
 단순 수학, 구현 문제
 
 단순한 수학, 구현 문제이다.
+    x / a = 올림한 값 i
+ㄴ> x = (i - 1) * a + 1 --> i에서 1빼는 다시 더하는 이유는 올림한 값이므로 뺀 후에 다시 더해주면 된다.
+
+단순하게 반복문으로 풀어보면 아래와 같다. --> 입력값이 크지 않아서 가능하다..
+import math
+a, b = map(int,input().split())
+for x in range(100001):
+    if math.ceil(x / a) == i:
+        print(x)
+        break
 '''
 
 a, i = map(int, input().split())
@@ -11,4 +21,4 @@ a, i = map(int, input().split())
 # a, i = 1, 100 # 100
 # a, i = 10, 10 # 91
 
-print(a * (i - 1) + 1)
+print((i - 1) * a + 1)
