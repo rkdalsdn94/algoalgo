66bee4381baab155d14ea64ccbf5aee22c23da8e
minwooKang <rkdalsdn94@gmail.com>
2023-05-25 00:03:37 +0900

    refactor: 골드1 - 구간 곱 구하기 11505 - 세그먼트 트리 문제
    - 코드 중복 제거
      - merge 함수에서 return 할 때 % 1000000007 적용
    - 파일 상단 설명 추가
      - 해당 문제 링크 '출력'부분에 나머지 연산을 해야 된다는 정보가 있음. 이걸 못 보고 헤맴
      - % 1000000007 에 대한 재밌는 링크 추가

--- solved_ac/Gold_1/구간_곱_구하기_11505.py
+++ solved_ac/Gold_1/구간_곱_구하기_11505.py
@@ -7,6 +7,13 @@
 아래 코드에서 merge 할 때 다른 사람들의 코드는 % 1000000007 <-- 이 부분이 있어서 merge 하는 부분을 모두 저렇게 하니까 통과 되었다.
 파이썬이 느린건 알았지만 % 1000000007 이걸 했다고 통과하는게 신기했다. 나중에 찾아보려고 한다.
 
+문제 링크에 출력 부분을 보면 '첫째 줄부터 K줄에 걸쳐 구한 구간의 곱을 1,000,000,007로 나눈 나머지를 출력한다.' 이렇게 적혀있다... 문제를 잘 읽어야 된다.
+또한, % 1000000007 부분을 찾아보는 데 재밌는 링크가 있어 아래 첨부한다.
+https://sohyeonnn.tistory.com/36 위 링크를 요약하면 아래와 같다.
+- 연산자 분배 법칙에 의해 mod 결과 같은 값을 도출해 내기 위함
+- int overflow 방지
+- 소수로 나눠진 값이므로 정확도를 높일 수 있다.
+
 일단 풀이는 어제 풀었던 구간 곱 구하기이랑 똑같이 풀었다.
 단, 이 문제는 곱을 구해야 되는 문제이므로 merge 함수에서 곱셈을 활용하고, query 함수에서 범위에 벗어나는 부분은 1로 return 했다.
 합을 구하는 문제에선 merge 함수 부분을 따로 적진 않았지만 return 할 때 left_value + right_value 부분을 생각하면 된다.
@@ -26,7 +33,7 @@ command = [ list(map(int, input().split())) for _ in range(m + k) ]
 tree = [0] * n * 4
 
 def merge(left, right):
-    return left * right
+    return left * right % 1000000007
 
 def build(arr, node, left_node, right_node):
     if left_node == right_node:
@@ -36,7 +43,7 @@ def build(arr, node, left_node, right_node):
     mid = (left_node + right_node) // 2
     left_value = build(arr, node * 2, left_node, mid)
     right_value = build(arr, node * 2 + 1, mid + 1, right_node)
-    tree[node] = merge(left_value, right_value) % 1000000007
+    tree[node] = merge(left_value, right_value)
     return tree[node]
 
 def update(index, new_value, node, left_node, right_node):
@@ -49,7 +56,7 @@ def update(index, new_value, node, left_node, right_node):
     mid = (left_node + right_node) // 2
     left_value = update(index, new_value, node * 2, left_node, mid)
     right_value = update(index, new_value, node * 2 + 1, mid + 1, right_node)
-    tree[node] = merge(left_value, right_value) % 1000000007
+    tree[node] = merge(left_value, right_value)
     return tree[node]
 
 def query(left, right, node, left_node, right_node):
@@ -61,7 +68,7 @@ def query(left, right, node, left_node, right_node):
     mid = (left_node + right_node) // 2
     left_value = query(left, right, node * 2, left_node, mid)
     right_value = query(left, right, node * 2 + 1, mid + 1, right_node)
-    return merge(left_value, right_value) % 1000000007
+    return merge(left_value, right_value)
 
 build(n_list, 1, 0, n - 1)
 for i in command:
