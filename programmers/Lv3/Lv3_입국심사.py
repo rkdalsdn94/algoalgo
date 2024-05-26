89e48c0b90875628494b29fc96b2a0a3427da161
minwooKang <rkdalsdn94@gmail.com>
2024-05-26 17:53:15 +0900

    프로그래머스 - Lv3 - 입국심사 - 이진 탐색 문제 문제를 다시 풀어봄

--- programmers/Lv3/Lv3_입국심사.py
+++ programmers/Lv3/Lv3_입국심사.py
@@ -1,3 +1,36 @@
+# 프로그래머스 - Lv3 - 입국심사 - 이진 탐색 문제
+'''
+이진 탐색 문제
+
+해당 문제를 다시 풀었다. 전에 풀었던 방식과 좀 다르게 최근에 푼 이진 탐색 방식처럼 풀었다.
+
+풀이 과정
+1. start, end를 1, times 중 가장 큰 값 * n으로 설정
+2. start가 end보다 작은 동안 반복
+    2.1. mid를 (start + end) // 2로 설정
+    2.2. cnt를 0으로 초기화하고, times를 돌면서 mid // i를 cnt에 더한다.
+    2.3. cnt가 n보다 크거나 같은 경우 end를 mid로 설정하고, 그렇지 않은 경우 start를 mid + 1로 설정한다.
+3. start를 반환한다.
+'''
+
+def solution(n, times):
+    start, end = 1, max(times) * n
+
+    while start < end:
+        mid = (start + end) // 2
+        cnt = sum([mid // i for i in times])
+
+        if cnt >= n:
+            end = mid
+        else:
+            start = mid + 1
+
+    return start
+
+print(solution(6, [7, 10])) # 28
+
+# 예전 코드
+'''
 def solution(n, times):
     answer = 0
     times.sort()
@@ -23,3 +56,4 @@ def solution(n, times):
     return answer
 
 print(solution(6, [7, 10])) # 28
+'''
